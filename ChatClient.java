ca56d56a88abead8a6026ac9e2118d1fc576395b
Yanan Chang <stanleychang81@gmail.com>
2016-06-23 10:29:00 -0400

    Chat 0.9

--- ChatClient.java
+++ ChatClient.java
@@ -1,7 +1,12 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.*;
+import java.io.*;
 
 public class ChatClient extends Frame {
+	Socket s = null;
+	DataOutputStream dos = null;
 
 	TextField tfTxt = new TextField();
 	TextArea taContent = new TextArea();
@@ -21,21 +26,55 @@ public class ChatClient extends Frame {
 
 			@Override
 			public void windowClosing(WindowEvent arg0) {
+				disconnect();
 				System.exit(0);
 			}
 			
 		});
 		tfTxt.addActionListener(new TFListener());
 		setVisible(true);
+		connect();
 	}
+	
+	public void connect(){
+		try {
+			s = new Socket("127.0.0.1",8888);
+			dos = new DataOutputStream(s.getOutputStream());
+System.out.println("Connected!");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void disconnect(){
+		try {
+			dos.close();
+			s.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
 	
 	private class TFListener implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			String s = tfTxt.getText().trim();
-			taContent.setText(s);
+			String str = tfTxt.getText().trim();
+			taContent.setText(str);
 			tfTxt.setText("");
+			try {
+				dos.writeUTF(str);
+				dos.flush();
+				//dos.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 			
 		}
 		
---
+++ ChatServer.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.io.*;
+import java.net.*;
+
+
+public class ChatServer {
+
+	
+	
+	public static void main(String[] args) {
+		boolean started = false;
+		ServerSocket ss = null;
+		Socket s = null;
+		DataInputStream dis = null;
+		try {
+			ss = new ServerSocket(8888);
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+		
+		try{
+			started = true;
+			while(started){
+				boolean bConnected = false;
+				s = ss.accept();
+System.out.println("a client connected!");
+				bConnected = true;
+				dis = new DataInputStream(s.getInputStream());
+				while(bConnected){
+				
+					String str = dis.readUTF();
+					System.out.println(str);
+				}
+				dis.close();
+
+			}
+		} catch (Exception e) {
+			
+			//e.printStackTrace();
+			System.out.println("Client closed!");
+		}finally{
+			try {
+				if(dis != null) dis.close();
+				if(s != null) s.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+	}
+
+}
